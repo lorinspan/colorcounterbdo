a20fe684bee5b632878643b65a15ddf204810678
lorinspan <lorinspanx@gmail.com>
2024-02-23 23:37:32 +0200

    gas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: []
 })
 export class AppComponent {
-  title = 'colorcounter';
+  colors: string[] = [];
+  colorCounts: { [key: string]: number } = {};
+
+  addColor(color: string): void {
+    if (!this.colorCounts[color]) {
+      this.colorCounts[color] = 1;
+    } else {
+      this.colorCounts[color]++;
+    }
+    this.updateColorsArray();
+  }
+
+  removeColor(color: string): void {
+    if (this.colorCounts[color] && this.colorCounts[color] > 0) {
+      this.colorCounts[color]--;
+      this.updateColorsArray();
+    }
+  }
+
+  updateColorsArray(): void {
+    this.colors = [];
+    for (const color in this.colorCounts) {
+      if (this.colorCounts[color] > 0) {
+        for (let i = 0; i < this.colorCounts[color]; i++) {
+          this.colors.push(color);
+        }
+      }
+    }
+  }
+
+  calculateWinner(): void {
+    let winner = 'white';
+    let maxCount = this.colorCounts['white'] || 0;
+
+    for (const color in this.colorCounts) {
+      if (this.colorCounts[color] && this.colorCounts[color] > maxCount) {
+        winner = color;
+        maxCount = this.colorCounts[color];
+      }
+    }
+
+    alert('Winner: ' + winner);
+  }
 }
